1ec9d0fa27e615022d3e67c378d096ea21c6f584
Robert Stine <rcorystine@gmail.com>
2020-12-18 00:34:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
-def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ")
-  reversed_sentence = new_sentence.each do |word|
-    word.reverse!
-  end
-  reversed_sentence.join(" ")
-end
+# def reverse_each_word(sentence)
+  #new_sentence = sentence.split(" ")
+  #reversed_sentence = new_sentence.each do |word|
+    #word.reverse!
+  #end
+  #reversed_sentence.join(" ")
+#end
+
